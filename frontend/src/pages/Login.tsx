480f1f9032090f214b7a64a93b13311e32074324
Deepika <12301540@students.koi.edu.au>
2025-11-16 22:34:27 +1100

    Added W2 and W3 tasks

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <p>This is where users will sign in to the system.</p>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
